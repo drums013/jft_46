dc20bcf7085071d7232c86bc2bfafa98f4f9eaf2
drums013 <trasher6@gmail.com>
2017-10-19 00:52:39 +0500

    Проджект "песочница"

---
+++ sandbox/src/main/java/ss12.java
@@ -0,0 +1,29 @@
+public class ss12 {
+
+  public static void main(String[] args) {
+
+    System.out.println("       #####################         ");
+    System.out.println("     ######             ######       ");
+    System.out.println("    #####                 ######     ");
+    System.out.println("   ####                     #####    ");
+    System.out.println("  ####                        #####  ");
+    System.out.println(" ####     ####        ####      ###  ");
+    System.out.println(" ###     ######      ######     #### ");
+    System.out.println("####     ######      ######      ####");
+    System.out.println("###       ####        ####       ####");
+    System.out.println("###                              ####");
+    System.out.println("###                              ####");
+    System.out.println("###                              ####");
+    System.out.println("###     ###              ###     ####");
+    System.out.println("####     ##################      ####");
+    System.out.println(" ###                             ####");
+    System.out.println(" ####                            ### ");
+    System.out.println("  ####                         ##### ");
+    System.out.println("   ####                      #####   ");
+    System.out.println("    #####                 ######     ");
+    System.out.println("     #########################       ");
+    System.out.println("      #####################          ");
+
+ }
+
+}
