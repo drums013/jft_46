a636669e950282c3f45ab3c63325e029ac10473e
drums013 <trasher6@gmail.com>
2017-10-24 15:28:26 +0500

    Вычисление площади

---
+++ sandbox/src/main/java/training/pft/sanbox/Square.java
@@ -0,0 +1,15 @@
+package training.pft.sanbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return  this.l * this.l;
+  }
+
+}
