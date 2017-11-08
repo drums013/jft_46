4757570ecb47bcb7799246973bf28f0f9e4d8991
drums013 <trasher6@gmail.com>
2017-11-09 02:30:48 +0500

    Задание#8. Реализована проверка и обеспечение выполнения предусловий для удаления и модификации контактов

--- addressbook-web-tests/src/test/java/training/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/training/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,12 +1,19 @@
 package training.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import training.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion() {
     app.getNavigationHelper().gotoHomePage();
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("Vasya", "Petrov",
+              "Petrovich", "tester.1123", "+7(343)00-00-00", "+7(900)949-04-04",
+              "8-800-000-00-00", "tester1@rrr.com", "tester2@gmail.com",
+              "tester@666.ru", "http://localhost","[none]"), true);
+    }
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContacts();
     app.getNavigationHelper().gotoHomePage();
--- addressbook-web-tests/src/test/java/training/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/training/pft/addressbook/tests/ContactModificationTests.java
@@ -8,8 +8,17 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification(){
     app.getNavigationHelper().gotoHomePage();
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData(null, null,
+              null, null, null, null,
+              null, null, null,
+              null, null, "[none]"), true);
+    }
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillOutContactDetails(new ContactData("Vasya", "Petrov", "Petrovich", "tester.1123", "+7(343)00-00-00", "+7(900)949-04-04", "8-800-000-00-00", "tester1@rrr.com", "tester2@gmail.com", "tester@666.ru", "http://localhost", null),false);
+    app.getContactHelper().fillOutContactDetails(new ContactData("Vasya", "Petrov",
+            "Petrovich", "tester.1123", "+7(343)00-00-00", "+7(900)949-04-04",
+            "8-800-000-00-00", "tester1@rrr.com", "tester2@gmail.com",
+            "tester@666.ru", "http://localhost","[none]"),false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
   }
