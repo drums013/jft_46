8b91c0e9dd4ccb9335a80b739caa10bbb0cf2f07
drums013 <trasher6@gmail.com>
2017-11-08 22:11:34 +0500

    Реализована возможность запуска тестов в разных браузерах

--- addressbook-web-tests/src/test/java/training/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/training/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package training.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import training.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
