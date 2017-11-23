cfeb03888cd97fcea6fc89758314486536b4a707
drums013 <trasher6@gmail.com>
2017-11-23 23:23:45 +0500

    Добавлено кеширования для длительных операций

--- addressbook-web-tests/src/test/java/training/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/training/pft/addressbook/appmanager/ContactHelper.java
@@ -70,6 +70,7 @@ public class ContactHelper extends HelperBase {
     gotoAddContactPage();
     fillOutContactDetails(contact, bool);
     submitContactDetails();
+    contactCache = null;
     returnToHomePage();
   }
 
@@ -77,12 +78,14 @@ public class ContactHelper extends HelperBase {
     initContactModificationById(contact.getId());
     fillOutContactDetails((contact), false);
     submitContactModification();
+    contactCache = null;
     returnToHomePage();
   }
 
   public void delete(ContactData contact) {
     selectContactById(contact.getId());
     deleteSelectedContacts();
+    contactCache = null;
   }
 
 
@@ -94,17 +97,22 @@ public class ContactHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
+  private Contacts contactCache = null;
+
   public Contacts all() {
-    Contacts contacts = new Contacts();
+    if (contactCache != null) {
+      return new Contacts(contactCache);
+    }
+    contactCache = new Contacts();
     List<WebElement> elements = wd.findElements(By.name("entry"));
     for (WebElement element : elements) {
       List<WebElement> cells = element.findElements(By.tagName("td"));
       String firstname = cells.get(2).getText();
       String lastname = cells.get(1).getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      contacts.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
+      contactCache.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
     }
-    return contacts;
+    return new Contacts(contactCache);
   }
 
 }
