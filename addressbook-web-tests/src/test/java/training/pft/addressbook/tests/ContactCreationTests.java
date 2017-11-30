d609343687018a3445622267beed7c3cec80bccf
drums013 <trasher6@gmail.com>
2017-11-30 23:14:12 +0500

    Параметризованы методы для тестов создания групп и контактов

--- addressbook-web-tests/src/test/java/training/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/training/pft/addressbook/tests/ContactCreationTests.java
@@ -1,25 +1,40 @@
 package training.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import training.pft.addressbook.model.ContactData;
 import training.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    File photo = new File("src/test/resources/test.png");
+    list.add(new Object[] {new ContactData().withFirstname("Vasya 1").withMiddlename("Petrov 1").withLastname("Petrovich 1")
+            .withNick("tester.1").withHomephone("11-11-11").withMobilephone("+7(900)111 11 11")
+            .withWorkphone("8-800-000-00-00").withFirstmail("tester1@rrr.com").withSecondmail("tester2@gmail.com")
+            .withFirdmail("tester@666.ru").withUserhomepage("http://localhost").withPhoto(photo)});
+    list.add(new Object[] {new ContactData().withFirstname("Dany").withLastname("Jones")
+            .withHomephone("11-11-11").withMobilephone("+7(900)111 11 11")
+            .withWorkphone("8-800-000-00-00")});
+    list.add(new Object[] {new ContactData().withFirstname("Dany 2").withLastname("Jones 2")
+            .withFirstmail("111@1.com").withSecondmail("222@2.com").withFirdmail("333@3.cpom")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/test.png");
-    ContactData contact = new ContactData().withFirstname("Vasya").withMiddlename("Petrov").withLastname("Petrovich")
-            .withNick("tester.1123").withHomephone("+7(343)00-00-00").withMobilephone("+7(900)949-04-04")
-            .withWorkphone("8-800-000-00-00").withFirstmail("tester1@rrr.com").withSecondmail("tester2@gmail.com")
-            .withFirdmail("tester@666.ru").withUserhomepage("http://localhost").withPhoto(photo);
     app.contact().create(contact, true);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
