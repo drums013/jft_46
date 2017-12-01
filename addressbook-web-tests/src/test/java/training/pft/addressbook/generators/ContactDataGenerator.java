e79c1c893178575a81416afd796aab0a70661563
drums013 <trasher6@gmail.com>
2017-12-01 20:09:10 +0500

    Реализовано автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/training/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/training/pft/addressbook/generators/ContactDataGenerator.java
@@ -54,28 +54,29 @@ public class ContactDataGenerator {
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+
+    }
   }
 
   private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
 
   private static void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname()
-              , contact.getHomephone(), contact.getMobilephone(), contact.getWorkphone(), contact.getFirstmail()
-              , contact.getSecondmail(), contact.getFirdmail()));
+    try (Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname()
+                , contact.getHomephone(), contact.getMobilephone(), contact.getWorkphone(), contact.getFirstmail()
+                , contact.getSecondmail(), contact.getFirdmail()));
+      }
     }
-    writer.close();
   }
 
   private static List<ContactData> generateContacts(int count) {
