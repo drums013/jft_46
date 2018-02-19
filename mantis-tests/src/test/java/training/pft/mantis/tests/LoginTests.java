fc4fa8bf23edd63fdf27bcc08650f24a865ed2c7
drums013 <trasher6@gmail.com>
2018-02-19 23:39:08 +0500

    Реализована ленивая инициализация помощников

---
+++ mantis-tests/src/test/java/training/pft/mantis/tests/LoginTests.java
@@ -0,0 +1,18 @@
+package training.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+import training.pft.mantis.appmaneger.HttpSession;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends TestBase {
+
+  @Test
+  public void testLogin() throws IOException {
+    HttpSession session = app.newSession();
+    assertTrue(session.login("administrator", "root"));
+    assertTrue(session.isLoggedInAs("administrator"));
+  }
+}
