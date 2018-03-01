6c18c64c12a3fe8c8eae76bcf8ca274576faf725
drums013 <trasher6@gmail.com>
2018-03-01 22:59:24 +0500

    Взаимодействие с github

---
+++ github-sample/src/test/java/training/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package training.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("9520aa48d745f94ed907d9c008022d13227ee019");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("drums013", "jft_46")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
